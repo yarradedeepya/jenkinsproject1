d638549028ef625ead723545de3ccbf13815767d
dd <dedeepya.dd@gmail.com>
2021-07-22 15:16:11 +0530

    jenkins file added

---
+++ jenkins.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/yarradedeepya/Hello-World.git'
+                }
+            }
+        stage('Test'){
+            steps{
+                sh "python tinker.py
+		echo "sucess"
+            
+            }
+            
+        }
+        stage('Deploy'){
+            steps{
+            println "deployed"
+            }
+        }
+    }
+    
