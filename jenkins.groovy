d53af2de2b79787671a209a4642969d0449b0ec1
dd <dedeepya.dd@gmail.com>
2021-07-22 15:20:38 +0530

    jenkins file modified

--- jenkins.groovy
+++ jenkins.groovy
@@ -9,7 +9,7 @@ pipeline {
             }
         stage('Test'){
             steps{
-                sh "python tinker.py
+                sh "python tinker.py"
 		echo "sucess"
             
             }
