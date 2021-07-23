4519761078b095a49e3a2d24cf8e9d4b566d5b75
dd <dedeepya.dd@gmail.com>
2021-07-23 09:30:13 +0530

    jenkines file modifed

--- jenkins.groovy
+++ jenkins.groovy
@@ -11,7 +11,7 @@ pipeline {
         stage('Test'){
             steps{
 		echo "in Test"
-                sh "Python tinker.py"
+                sh "pwd"
 		echo "sucess"
             
             }
